d90a51b127d80f852df0eea7c5a3325c9f8177c7
qiyei2015 <1273482124@qq.com>
2020-08-05 11:06:46 +0800

    v2.4-1 编程式事务

--- src/main/java/com/qiyei/spring/demo/JdbcDemoApplication.java
+++ src/main/java/com/qiyei/spring/demo/JdbcDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -29,6 +30,7 @@ public class JdbcDemoApplication implements CommandLineRunner {
     private FooDao mFooDao;
 
     @Autowired
+    @Lazy
     private BatchFooDao mBatchFooDao;
 
     public static void main(String[] args) {
---
+++ src/main/java/com/qiyei/spring/demo/ProgrammaticTransactionDemoApplication.java
@@ -0,0 +1,50 @@
+package com.qiyei.spring.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ * @author Created by qiyei2015 on 2020/8/5.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+@SpringBootApplication
+@Slf4j
+public class ProgrammaticTransactionDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private TransactionTemplate mTransactionTemplate;
+    @Autowired
+    private JdbcTemplate mJdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammaticTransactionDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("count before Transaction =" + getCount());
+        mTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                mJdbcTemplate.execute("insert into FOO (ID,BAR) values (3,'hhhh')");
+                log.info("count in Transaction =" + getCount());
+                transactionStatus.setRollbackOnly();
+            }
+        });
+        log.info("count after Transaction =" + getCount());
+    }
+
+    private long getCount() {
+        return (long) mJdbcTemplate.queryForList("SELECT COUNT(*) AS CNT FROM FOO")
+                .get(0).get("CNT");
+    }
+}
